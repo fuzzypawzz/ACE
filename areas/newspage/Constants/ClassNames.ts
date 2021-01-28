75f870f4eba5ca20f67c18d338271b7d575154e0
Jannik Maag <jannikmaag@outlook.com>
2021-01-28 18:50:54 +0100

    Correct className constants

--- areas/newspage/Constants/ClassNames.ts
+++ areas/newspage/Constants/ClassNames.ts
@@ -1,3 +1,3 @@
 export default class ClassNames {
-  static readonly newsWrapperBlock: string = "_newsSection-wrapper";
+  static readonly newsBlockWrapper: string = "_newsSection-wrapper";
 }
--- areas/newspage/Constants/NewsItemClassNames.ts
+++ areas/newspage/Constants/NewsItemClassNames.ts
@@ -1,12 +1,14 @@
 export default class NewsItemClassNames {
   static readonly wrapper: string = "newsblock-01-wrapper";
   static readonly header: string = "newsblock-01-header";
-  static readonly logoWrapper: string = "newsblock-01-header";
-  static readonly svg: string = "newsblock-01-header";
-  static readonly authorAndDateWrapper: string = "newsblock-01-header";
-  static readonly author: string = "newsblock-01-header";
-  static readonly date: string = "newsblock-01-header";
-  static readonly newsContentWrapper: string = "newsblock-01-header";
-  static readonly newsContentHeadline: string = "newsblock-01-header";
-  static readonly newsContentBody: string = "newsblock-01-header";
+  static readonly logoWrapper: string = "newsblock-01-logo";
+  static readonly svg: string = "newsblock-01-svg";
+  static readonly authorAndDateWrapper: string = "newsblock-01-infos";
+  static readonly author: string =
+    "newsblock-01-headline news-headline pebble-purple";
+  static readonly date: string = "newsblock-01-date";
+  static readonly newsContentWrapper: string = "newsblock-01-content";
+  static readonly newsContentHeadline: string = "newsblock-content-headline";
+  static readonly newsContentBody: string = "newsblock-content-p";
+  static readonly newsContentPhotos: string = "newsblock-01-photos";
 }
