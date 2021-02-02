f838fc6e99c6181677d906220d393f005e472681
Jannik Maag <jannikmaag@outlook.com>
2021-02-02 17:55:42 +0100

    Create TableKeys.ts

---
+++ areas/newspage/Constants/TableKeys.ts
@@ -0,0 +1,10 @@
+export default class TableKeys {
+  static readonly DAY: string = "dag";
+  static readonly MONTH: string = "måned";
+  static readonly YEAR: string = "år";
+  static readonly AUTHOR: string = "afsender";
+  static readonly HEADLINE: string = "overskrift";
+  static readonly CONTENT_TEXT: string = "tekst";
+  static readonly IMG: string = "billede";
+  static readonly HREF: string = "link";
+}
